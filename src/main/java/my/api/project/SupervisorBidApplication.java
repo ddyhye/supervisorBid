97e3f60ad45a8614d5e48beb4d2a3f4716ff706e
dd_hye <ddy.hye@gmail.com>
2024-08-05 15:17:16 +0900

    충돌 해결,,

---
+++ src/main/java/my/api/project/SupervisorBidApplication.java
@@ -0,0 +1,13 @@
+package my.api.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisorBidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupervisorBidApplication.class, args);
+	}
+
+}
